a1148f367412a8a44c81517f700be2d7afe6812a
Jimmy Yu <jimmyu219@hotmail.com>
2015-03-03 18:26:22 -0800

    Translated image files to text files so we can use them in C Code
    Used image_converter.m script to convert img.jpg to img.txt

---
+++ images/image_converter.m
@@ -0,0 +1,11 @@
+image = imread('img25.jpg');    % take this image
+gray = rgb2gray(image);         % grayscale it
+image = imresize(image, [480 640]); % resize it 480 x 640
+
+file = fopen('img25.txt', 'wt');    % make a text file
+for i = 1:640
+    for j = 1:480
+        fprintf(file, '%d ', image(j,i));
+    end
+end
+fclose(file);
