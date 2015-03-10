060e5f12ce6ff6435b1552bba2f915032154136b
Jimmy Yu <jimmyu219@hotmail.com>
2015-03-10 16:15:15 -0700

    finished roi.c translation
    Finished the skeleton of the c code; now just need to debug it and have it work on altera monitor. At the
    moment, fstream is not working in Altera Monitor. When trying to read
    a text file there, it freezes.

--- read.c
+++ read.c
@@ -1,3 +1,5 @@
+// converting roi.m to roi.c
+
 # include <stdio.h>
 
 
@@ -14,6 +16,9 @@ int main(){
 	int row_max = 0, rows_avg = 0;
 	int rows_level = 0;
 	int cols_level = 0;
+	int row_index_beg, row_index_end;
+	int col_index_beg, col_index_end;
+
 
 	// Generate image in an array
 	// sum rows and cols in arrays
@@ -24,6 +29,11 @@ int main(){
 			rows_sum[rows] += img[rows][cols];
 		}
 	}
+
+//-------------------------------------------------------------------------------------
+//		Get black ROI image
+//-------------------------------------------------------------------------------------
+
 
 	// Find row_max and col_max
 	// Calculate row_avg and col_avg
@@ -50,7 +60,8 @@ int main(){
 	cols_level = (cols_max + cols_avg)/2;
 	rows_level = (rows_max + rows_avg)/2;
 
-	// Make im2bw arrays for rows and cols
+	// Make im2bw arrays
+	// for cols
 	for (cols = 0; cols < 640; cols++) {
 		if (cols_level <= cols_sum[cols])
 			cols_binary[cols] = 1;
@@ -58,7 +69,7 @@ int main(){
 			cols_binary[cols] = 0;
 	}
 		
-
+	// for rows
 	for (rows = 0; rows < 480; rows++) {
 		if (rows_level <= rows_sum[rows])
 			rows_binary[rows] = 1;
@@ -67,9 +78,38 @@ int main(){
 	}
 
 
+	// Find the array where row_index == first part of black box
+	// row_index(end) should be the end of the black box
+	for (rows = 0; rows < 480; rows++) {
+		if (rows_binary[rows] == 0)
+			row_index_beg = rows;
+			
+	}
+
+	for (rows = 480; rows < 0; rows--) {
+		if (rows_binary[rows] == 0)
+			row_index_end = rows;
+			
+	}
+
+
+	// Find the array where col_index == first part of black box
+	// col_index(end) should be the end of the black box 
+	for (cols = 0; cols < 640; cols++) {
+		if (cols_binary[cols] == 0)
+			col_index_beg = cols;
+	}
+
+	for (cols = 640; cols < 0; cols--) {
+		if (cols_binary[cols] == 0)
+			col_index_end = cols;
+	}
 
 
 	fclose(file);
+
+	// print img matrix array
+	// should be just the black box with the digit
 	for (cols = 0; rows < 640; cols++) {
 		printf("\n");
 		for (rows = 0; rows < 480; rows++) {
