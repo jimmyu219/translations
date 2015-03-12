26a706e7471c3f7673ed85cd8a79f51a662d4882
Jimmy Yu <jimmyu219@hotmail.com>
2015-03-11 20:42:48 -0700

    more debugging
    going to work at home

--- images/image_converter.m
+++ images/image_converter.m
@@ -1,8 +1,8 @@
-image = imread('img25.jpg');    % take this image
+image = imread('image1.jpg');    % take this image
 gray = rgb2gray(image);         % grayscale it
 image = imresize(image, [480 640]); % resize it 480 x 640
 
-file = fopen('img25.txt', 'wt');    % make a text file
+file = fopen('image1.txt', 'wt');    % make a text file
 for i = 1:640
     for j = 1:480
         fprintf(file, '%d ', image(j,i));
